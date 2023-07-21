7bf5e871f45a6690dd764832ad3705feef9912fb
Rajeevan <rajee.srs@gmail.com>
2023-07-20 18:58:08 -0700

    Add Layout

--- src/App.js
+++ src/App.js
@@ -1,17 +1,11 @@
-import React from 'react';
+import React from "react";
+import Layout from "./components/Layout/Layout";
 
 function App() {
   return (
-    <div style={{ textAlign: 'center' }}>
-      <header>
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a href="https://reactjs.org" target="_blank" rel="noopener noreferrer">
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <Layout />
+    </>
   );
 }
 
