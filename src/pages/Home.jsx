093c71b9f2ed79e0193e9c10693a49c6301b0d13
Rajeevan <rajee.srs@gmail.com>
2023-07-23 11:24:00 -0700

    Home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,7 +1,56 @@
 import React from "react";
+import { Col, Container, Row } from "reactstrap";
+import heroImg from "../assets/images/hero-img01.jpg";
+import heroImg02 from "../assets/images/hero-img02.jpg";
+import heroVideo from "../assets/images/hero-video.mp4";
+import worldImg from "../assets/images/world.png";
+import "../styles/home.css";
+import Subtitle from "./../shared/Subtitle";
 
 const Home = () => {
-  return <div>Home</div>;
+  return (
+    <>
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="hero__content">
+                <div className="hero__subtitle d-flex align-items-center">
+                  <Subtitle substitle={"Know Before You Go"} />
+                  <img src={worldImg} alt="" />
+                </div>
+                <h1>
+                  Travelling opens the door to creating
+                  <span className="highlight"> memories </span>
+                </h1>
+                <p>
+                  Lorem ipsum dolor sit amet consectetur, adipisicing elit.
+                  Eligendi ab distinctio obcaecati expedita maxime provident
+                  saepe repudiandae itaque! Esse placeat qui tenetur incidunt
+                  accusamus maiores.
+                </p>
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box">
+                <img src={heroImg} alt="" />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-4">
+                <video src={heroVideo} alt="" controls />
+              </div>
+            </Col>
+            <Col lg="2">
+              <div className="hero__img-box mt-5">
+                <img src={heroImg02} alt="" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+    </>
+  );
 };
 
 export default Home;
