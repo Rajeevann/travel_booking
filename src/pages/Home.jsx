eab4347f61f34bd01f66ad171ce790260c210e09
Rajeevan <rajee.srs@gmail.com>
2023-07-25 11:27:25 -0700

    Add Experice Section

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -4,6 +4,7 @@ import heroImg from "../assets/images/hero-img01.jpg";
 import heroImg02 from "../assets/images/hero-img02.jpg";
 import heroVideo from "../assets/images/hero-video.mp4";
 import worldImg from "../assets/images/world.png";
+import experience from "../assets/images/experience.png";
 import "../styles/home.css";
 import Subtitle from "./../shared/Subtitle";
 import SearchBar from "../shared/SearchBar";
@@ -81,6 +82,59 @@ const Home = () => {
         </Container>
       </section>
       {/* =========== Feature Section Start */}
+
+      {/* =========== Experience Section Start */}
+      <section>
+        <Container>
+          <Row>
+            <Col lg="6">
+              <div className="experience__content">
+                <Subtitle subtitle={"Experience"} />
+                <h2>
+                  With our all experience
+                  <br /> we will serve you
+                </h2>
+                <p>
+                  Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                  <br /> Odit, eligendi magnam. Reprehenderit corporis aliquam
+                  explicabo!
+                </p>
+              </div>
+              <div className="counter__wrapper d-flex align-items-center gap-5">
+                <div className="counter__box">
+                  <span>12k+</span>
+                  <h6>Sucessful Trip</h6>
+                </div>
+                <div className="counter__box">
+                  <span>2k+</span>
+                  <h6>Regular Clients</h6>
+                </div>
+                <div className="counter__box">
+                  <span>15</span>
+                  <h6>Years Experience</h6>
+                </div>
+              </div>
+            </Col>
+            <Col lg="6">
+              <div className="experience__img">
+                <img src={experience} alt="experience" />
+              </div>
+            </Col>
+          </Row>
+        </Container>
+      </section>
+      {/* =========== Experience Section End ============*/}
+      {/* =========== Gallery Section Start ============*/}
+      <Container>
+        <Row>
+          <Col lg="12">
+            <Subtitle subtitle={"Gallery"} />
+            <h2 className="gallery__title">Visit our customers tour gallery</h2>
+          </Col>
+          <Col lg="12"></Col>
+        </Row>
+      </Container>
+      {/* =========== Gallery Section End ============*/}
     </>
   );
 };
