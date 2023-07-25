f66b7aa7a98777a280fe33aaedd991c435a134d5
Rajeevan <rajee.srs@gmail.com>
2023-07-25 14:41:12 -0700

    Add Footer

--- src/components/Footer/Footer.jsx
+++ src/components/Footer/Footer.jsx
@@ -1,7 +1,138 @@
 import React from "react";
+import { Container, Row, Col, ListGroup, ListGroupItem } from "reactstrap";
+import logo from "../../assets/images/logo.png";
+import { Link } from "react-router-dom";
+import "./footer.css";
+
+const quick__links = [
+  {
+    path: "/home",
+    display: "Home",
+  },
+  {
+    path: "/about",
+    display: "About",
+  },
+  {
+    path: "/tours",
+    display: "Tours",
+  },
+];
+
+const quick__links2 = [
+  {
+    path: "/gallery",
+    display: "Gallery",
+  },
+  {
+    path: "/login",
+    display: "Login",
+  },
+  {
+    path: "/register",
+    display: "Register",
+  },
+];
 
 const Footer = () => {
-  return <div>Footer</div>;
+  const year = new Date().getFullYear;
+
+  return (
+    <footer className="footer">
+      <Container>
+        <Row>
+          <Col lg="3">
+            <div className="logo">
+              <img src={logo} alt="" />
+              <p>
+                Lorem ipsum dolor sit amet, consectetur adipisicing elit.
+                Maiores, dolorum?
+              </p>
+              <div className="social__links d-flex align-items-center gap-4">
+                <span>
+                  <Link to="#">
+                    <i class="ri-youtube-line" />
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-github-line" />
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-facebook-line" />
+                  </Link>
+                </span>
+                <span>
+                  <Link to="#">
+                    <i class="ri-linkedin-line" />
+                  </Link>
+                </span>
+              </div>
+            </div>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__links-title">Discover</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__links.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__links-title">Quick Links</h5>
+            <ListGroup className="footer__quick-links">
+              {quick__links2.map((item, index) => (
+                <ListGroupItem key={index} className="ps-0 border-0">
+                  <Link to={item.path}>{item.display}</Link>
+                </ListGroupItem>
+              ))}
+            </ListGroup>
+          </Col>
+          <Col lg="3">
+            <h5 className="footer__links-title">Contact</h5>
+            <ListGroup className="footer__quick-links">
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-map-pin-line" />
+                  </span>
+                  Address:
+                </h6>
+                <p className="mb-0">123 Cambie </p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-mail-line" />
+                  </span>
+                  Email:
+                </h6>
+                <p className="mb-0">test@test.com </p>
+              </ListGroupItem>
+              <ListGroupItem className="ps-0 border-0 d-flex align-items-center gap-3">
+                <h6 className="mb-0 d-flex align-items-center gap-2">
+                  <span>
+                    <i class="ri-phone-line" />
+                  </span>
+                  Phone:
+                </h6>
+                <p className="mb-0">+123-456-789</p>
+              </ListGroupItem>
+            </ListGroup>
+          </Col>
+          <Col lg="12" className="text-center pt-5">
+            <p className="copyright">
+              Copyright {year}, Designed & Developed By Solid Analytics
+            </p>
+          </Col>
+        </Row>
+      </Container>
+    </footer>
+  );
 };
 
 export default Footer;
