fec85272f103f70f992a3fe57ffbddecc925fe7e
Rajeevan <rajee.srs@gmail.com>
2023-07-25 16:11:02 -0700

    Add Sticky Bar

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
 import { Container, Row, Button } from "reactstrap";
 import logo from "../../assets/images/logo.png";
 import { Link, NavLink } from "react-router-dom";
@@ -20,8 +20,53 @@ const nav_links = [
 ];
 
 const Header = () => {
+  // const headerRef = useRef(null);
+
+  // const stickyHeaderFunc = () => {
+  //   window.addEventListener("scroll", () => {
+  //     if (
+  //       document.body.scrollTop > 80 ||
+  //       document.documentElement.scrollTop > 80
+  //     ) {
+  //       headerRef.current.classList.add("sticky__header");
+  //     } else {
+  //       headerRef.current.classList.remove("sticky__header");
+  //     }
+  //   });
+  // };
+
+  // useEffect(() => {
+  //   stickyHeaderFunc();
+
+  //   return () => {
+  //     window.removeEventListener("scroll", stickyHeaderFunc);
+  //   };
+  // }, []);
+
+  const headerRef = useRef(null);
+
+  const stickyHeaderFunc = () => {
+    if (
+      document.body.scrollTop > 80 ||
+      document.documentElement.scrollTop > 80
+    ) {
+      headerRef.current?.classList.add("sticky__header");
+    } else {
+      headerRef.current?.classList.remove("sticky__header");
+    }
+  };
+
+  useEffect(() => {
+    window.addEventListener("scroll", stickyHeaderFunc);
+
+    return () => {
+      // Clean up the event listener when the component is unmounted
+      window.removeEventListener("scroll", stickyHeaderFunc);
+    };
+  }, []);
+
   return (
-    <header className="header">
+    <header className="header" ref={headerRef}>
       <Container>
         <Row>
           <div className="nav__wrapper d-flex align-items-center justify-content-between">
